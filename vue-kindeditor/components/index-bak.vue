66d19d23d893371dbfca41ee6dbc1cbfa552825a
午夜小白鼠 <1048523306@qq.com>
2019-09-16 19:57:37 +0800

    Update index-bak.vue

--- vue-kindeditor/components/index-bak.vue
+++ vue-kindeditor/components/index-bak.vue
@@ -1,34 +1,26 @@
 <template>
-  <div class='kindeditor'>
+  <div class="kindeditor">
     <textarea 
-      class='textarea' 
-      ref="kindeditor"
-      :id="kindeditorId"></textarea>
+      class="textarea" 
+      ref="kindeditor" 
+      v-model="localValue" 
+      name="content"></textarea>
   </div>
 </template>
 <script>
-/**
- * kindeditor 富文本编辑器组件
- * http://kindeditor.net/doc.php
- */
-
 import '../../../node_modules/kindeditor/kindeditor-all.js'
 import '../../../node_modules/kindeditor/lang/zh-CN.js'
 import '../../../node_modules/kindeditor/themes/default/default.css'
 
 export default {
   name: 'kindeditor',
-  components: {},
   props: {
-    id: {
-      type: String
+    options: {
+      type: Object,
     },
     value: {
       type: String,
       default: ''
-    },
-    options: {
-      type: Object
     }
   },
   data() {
@@ -36,64 +28,63 @@ export default {
       editor: null,
       hasChange: false,
       hasInit: false,
-      kindeditorId: this.id || 'vue-ke-' + +new Date()
+      localValue: ''
     }
   },
   watch: {
-    value(val) {
-      if (this.hasChange && this.hasInit) {
-        console.log('=====',val)
-        this.$nextTick(() => this.editor.html(val))
-      }
-    }
+    localValue: 'updateValue',
+    value: 'setDefault'
+  },
+  created() {
   },
   mounted() {
-    const self = this
-    // 默认参数
-    var options = {
-      uploadJson: 'upload/image',
-      width: '100%',
-      height: '300',
-      //设置编辑器创建后执行的回调函数
-      afterCreate: function() {
-        if (self.value) {
-          self.editor.html(self.value)
+    this.initKindeditor()
+  },
+  methods: {
+    initKindeditor() {
+      var self = this
+      // 默认参数
+      var options = {
+          uploadJson: 'http://localhost:3000/apis/article/kindeditor?flag=3',
+          width: '100%',
+          height: '200',
+          afterCreate: function() {
+            if (self.value) {
+              self.localValue = self.value
+            }
+            self.hasInit = true
+          },
+          //编辑器内容发生变化后执行的回调函数
+          afterChange: function() {
+            self.hasChange = true
+          }
         }
-        self.hasInit = true
-      },
-      //编辑器内容发生变化后执行的回调函数
-      afterChange: function() {
-        self.hasChange = true
-      },
-      //编辑器失去焦点(blur)时执行的回调函数
-      afterBlur: function() {
-        //将编辑器的内容设置到原来的textarea控件里
-        self.value = self.editor.html()
-        self.editor.sync()
+      // 调用外部参数
+      if (self.options) {
+        for (var i in self.options) {
+          options[i] = self.options[i]
+        }
+      }
+
+      self.editor = window.KindEditor.create(self.$refs.kindeditor, options);
+    },
+    // 设置初始值
+    setDefault() {
+      if (this.hasInit) {
+        this.$nextTick(function(){
+          this.localValue = this.value
+        })
+      }
+    },
+    // 修改父件的值
+    updateValue() {
+      if (this.hasChange && this.hasInit) {
+        this.$nextTick(function(){
+          this.editor.html(this.localValue)
+          this.$emit('input',this.localValue)
+        })
       }
     }
-    // 调用外部参数
-    if (self.options) {
-        for(var i in self.options){
-            options[i] = self.options[i]
-        }
-    }
-    self.editor = window.KindEditor.create(`#${this.kindeditorId}`, options)
-  },
-  destroyed() {
-    window.KindEditor.remove(`#${this.kindeditorId}`)
-    this.editor = null
   }
 }
 </script>
-<style rel="stylesheet/scss" lang="scss">
-.kindeditor {
-  position: relative;
-  margin-bottom: 30px;
-}
-
-.textarea {
-  visibility: hidden;
-  z-index: -1;
-}
-</style>
