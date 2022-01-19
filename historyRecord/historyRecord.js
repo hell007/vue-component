a6da47c2b2ea746ba5f40ccddf9a2db0e750f54e
午夜小白鼠 <1048523306@qq.com>
2022-01-19 09:22:27 +0800

    Update historyRecord.js

--- historyRecord/historyRecord.js
+++ historyRecord/historyRecord.js
@@ -1,16 +1,30 @@
-import { isEmpty, deepClone } from '@/dragComponents/utils'
+import { isEmpty, deepClone, isEqual } from '@/dragComponents/utils'
 
 const historyOptions = {
-  maxStack: 20 // 最多20步
+  key: 'history-records',
+  maxStack: 20, // 最多20步
+  autoUpdate: false, // 自动更新
+  delay: 10000 // 10s
+}
+
+function diff(value, other) {
+  return isEqual(value, other)
 }
 
 // eslint-disable-next-line no-unused-vars
 export default class HistoryRecord {
-  constructor() {
-    const { maxStack } = historyOptions
+  constructor(options) {
+    const { maxStack, autoUpdate, delay, key } = Object.assign(
+      historyOptions,
+      options
+    )
     this.actionPoints = [] // 快照数组
     this.step = 0 // 当前位置
     this.maxStack = maxStack // 最大步数
+    this.delay = delay
+    this.autoUpdate = autoUpdate
+    this.updateTime = 0 // 上次的操作时间
+    this.key = key
   }
 
   init(data) {
@@ -28,7 +42,22 @@ export default class HistoryRecord {
     return deepClone(data)
   }
 
-  push(data) {
+  push(data, type='add') {
+    const curTime = Date.now()
+    if (type === 'update') {
+      const equal = diff(data, this.actionPoints[this.step])
+      if (equal) return
+      if (this.autoUpdate) {
+        if (curTime - this.updateTime < this.delay) {
+          this.update(data)
+          return
+        }
+      } else {
+        this.update(data)
+        return
+      }
+    }
+
     if (this.step === this.maxStack - 1) {
       this.actionPoints.shift()
     } else {
@@ -39,16 +68,29 @@ export default class HistoryRecord {
     if (this.step < this.actionPoints.length - 1) {
       this.actionPoints = this.actionPoints.slice(0, this.step + 1)
     }
+    this.reset()
   }
 
   undo() {
     if (this.step !== 0) this.step--
+    this.reset()
     return deepClone(this.actionPoints[this.step])
   }
 
   redo() {
     if (this.step !== this.actionPoints.length - 1) this.step++
+    this.reset()
     return deepClone(this.actionPoints[this.step])
+  }
+  
+  update(data) {
+    this.step++
+    this.actionPoints[this.step] = deepClone(data)
+    this.reset()
+  }
+
+  reset() {
+    this.updateTime = Date.now()
   }
 
   // bindEvent() {
